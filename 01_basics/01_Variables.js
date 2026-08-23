5ca5e05f2750b233bea9b4361f0fd6756d1e0a5c
Shiva gautam <shivaji40581149@gmail.com>
2026-08-23 02:52:07 +0000

    Variables

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,12 @@
+const accountId = 40581
+let accountEmail = "Saksenaji88@gmail.com"
+var accountPassword = "983890"
+accountCity = "Jaipur"
+
+
+accountId =44444
+accountEmail = "Hello@gmail.com"
+accountCity = "Lucknow"
+accountPassword = 98989
+
+console.log(accountEmail);
